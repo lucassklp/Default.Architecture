316275eb20c671ee1cc5d51274a0041bf23bdf1a
Lucas Simas <simas.lucas@hotmail.com>
2017-12-07 16:07:55 -0300

    Json Standards and Swagger documentation

--- DefaultArchitecture/Startup.cs
+++ DefaultArchitecture/Startup.cs
@@ -13,6 +13,8 @@ using Repository.Interfaces;
 using Microsoft.Extensions.Logging;
 using Microsoft.ApplicationInsights.Extensibility;
 using Microsoft.ApplicationInsights.Extensibility.Implementation;
+using Swashbuckle.AspNetCore.Swagger;
+using Newtonsoft.Json.Serialization;
 
 namespace DefaultArchitecture
 {
@@ -36,6 +38,7 @@ namespace DefaultArchitecture
                     .AddDebug();
             });
 
+            //Disabling Telemery
             TelemetryConfiguration.Active.DisableTelemetry = true;
 
             //Configure the ConnectionString
@@ -43,7 +46,6 @@ namespace DefaultArchitecture
             {
                 options.UseMySql(Configuration.GetConnectionString("DefaultConnection"));
             });
-
 
             //Injecting the repositories
             services.AddTransient<IUserRepository, UserRepository>();
@@ -63,10 +65,9 @@ namespace DefaultArchitecture
             services.ConfigureJwtAuthentication();
             //Configuring Authorization
             services.ConfigureJwtAuthorization();
-            
 
             //All pages needs to be authenticated
-            services.AddMvc(config =>
+            var mvc = services.AddMvc(config =>
             {
                 var policy = new AuthorizationPolicyBuilder()
                              .RequireAuthenticatedUser()
@@ -74,18 +75,47 @@ namespace DefaultArchitecture
                 config.Filters.Add(new AuthorizeFilter(policy));
             });
 
+
+            mvc.AddJsonOptions(config =>
+            {
+                //All JSON returns lowerCamelCase (JSON Standard - By Google) instead of PascalCase (C# Standard - By Microsoft)
+                //References: 
+                //JSON Standards by Google https://google.github.io/styleguide/jsoncstyleguide.xml?showone=Property_Name_Format#Property_Name_Format
+                //C# Standards by Microsoft https://docs.microsoft.com/en-us/dotnet/standard/design-guidelines/capitalization-conventions
+                config.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+
+                //Trick for handling/ignoring Reference Loop Handling
+                config.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+
+            });
+
+            //Add the Swagger for API documentation
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "DefaultArchitecture API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            //Configure logger for show debug info.
             loggerFactory.AddConsole(LogLevel.Debug);
 
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-                
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "DefaultArchitecture V1");
+            });
+            
             app.UseCors("policy");
             app.UseAuthentication();
             app.UseMvc();
